e8e51c056e05e10b4c7872ca59366a5e86f48373
Owen Mathews <dembayago@allmail.net>
2025-03-28 11:39:50 -0700

    Updated README; added files.

---
+++ Merge.R
@@ -0,0 +1,103 @@
+# Renames fields in phase 1 data to match phase 3.
+phase1_prepped <- phase1 %>%
+    rename(
+        participants = actors,
+        claims_summary = claims,
+        event_type = type,
+        participant_injuries = injuries_crowd,
+        participant_casualties_any = injuries_crowd_any,
+        police_injuries = injuries_police,
+        police_casualties_any = injuries_police_any,
+        location = location_detail,
+        source1  = source_1,
+        source2  = source_2,
+        source3  = source_3,
+        source4  = source_4,
+        source5  = source_5,
+        source6  = source_6,
+        source7  = source_7,
+        source8  = source_8,
+        source9  = source_9,
+        source10 = source_10,
+        source11 = source_11,
+        source12 = source_12,
+        source13 = source_13,
+        source14 = source_14,
+        source15 = source_15,
+        source16 = source_16,
+        source17 = source_17,
+        source18 = source_18,
+        source19 = source_19,
+        source20 = source_20,
+        source21 = source_21,
+        source22 = source_22,
+        source23 = source_23,
+        source24 = source_24,
+        source25 = source_25,
+        source26 = source_26,
+        source27 = source_27,
+        source28 = source_28,
+        source29 = source_29,
+        source30 = source_30,
+    )
+
+# Merges the `issues` field of phase1 data into the `claims_summary` field.
+phase1_prepped$claims_summary <- paste(phase1_prepped$claims_summary, phase1_prepped$issues, sep="; ")
+
+# Deletes the `issues` field of phase 1 post-merge.
+phase1_prepped <- phase1_prepped %>%
+    select(
+        -issues
+    )
+
+# Renames fields in phase 2 data to match phase 3 and deletes fields related to issue tags, since they're not present in either of the other two data sets.
+phase2_prepped <- phase2 %>%
+    rename(
+        event_type = type,
+        participant_injuries = injuries_crowd,
+        participant_casualties_any = injuries_crowd_any,
+        police_injuries = injuries_police,
+        police_casualties_any = injuries_police_any,
+        location = location_detail,
+        source1  = source_1,
+        source2  = source_2,
+        source3  = source_3,
+        source4  = source_4,
+        source5  = source_5,
+        source6  = source_6,
+        source7  = source_7,
+        source8  = source_8,
+        source9  = source_9,
+        source10 = source_10,
+        source11 = source_11,
+        source12 = source_12,
+        source13 = source_13,
+        source14 = source_14,
+        source15 = source_15,
+        source16 = source_16,
+        source17 = source_17,
+        source18 = source_18,
+        source19 = source_19,
+        source20 = source_20,
+        source21 = source_21,
+        source22 = source_22,
+        source23 = source_23,
+        source24 = source_24,
+        source25 = source_25,
+        source26 = source_26,
+        source27 = source_27,
+        source28 = source_28,
+        source29 = source_29,
+        source30 = source_30,
+    ) %>%
+    select(
+        -issue_tags,
+        -issue_tags_summary,
+        -issue_tags_verbatim
+    )
+
+# For naming consistency.
+phase3_prepped <- phase3
+
+# Merge tables.
+fullData <- bind_rows(phase1_prepped, phase2_prepped, phase3_prepped)
--- README.md
+++ README.md
@@ -2,13 +2,19 @@
 
 Instructions and code for cleaning and rationalizing the three phases of [Crowd Counting Consortium][https://ash.harvard.edu/programs/crowd-counting-consortium/] data for use as a unified data set. This data is published as part of Harvard's [Ash Center for Democratic Justice and Innovation][https://ash.harvard.edu].
 
+If you can download the RData files directly from the links below, you won't need the contents of this repository; at the time of this writing, the RData download link wasn't working, returning this error:
+
+```
+{"status":"ERROR","code":404,"message":"datafile access error: requested optional service (image scaling, format conversion, etc.) could not be performed on this datafile."}
+```
+
 The data has already been cleaned and is published in three tranches:
 
 - [2017-2020][https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/6OPP7H]
 - [2021-2024][https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/9MMYDI]
 - [2025-][https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/RI9JFU]
 
-From the links above, you can download both data and associated coding guides.
+From these links, you can access both data and associated coding guides.
 
 ## Issues addressed in this repository
 
@@ -21,6 +27,18 @@ From the links above, you can download both data and associated coding guides.
 
 ## Contents
 
-XX contains Grep commands to fix the errors.
+`All fields summary.txt` contains comma-separated lists of all fields in each data set.
 
-YY contains R commands to import the data from each file, rationalize the tables, merge them into a single table, and export the results.
+`Guide to fields by phase.csv` summarizes all fields across the three phases and documents the changes (additions, deletions, etc.)
+
+`Cleanup.txt` contains Grep commands to fix the errors prior to import.
+
+`Setup.R` contains R commands to set up the environment.
+
+`Import phase <n>.R` contain R commands to import the data from each file.
+
+`Merge.R` contains R commands to rationalize the tables and merge them into a single table.
+
+## References
+
+- The [CCC issue dictionary][https://docs.google.com/document/d/1PHHfppFWOFeRs_fjpP64OBWqwQJB6AydIK9t8AZYR7w/edit?tab=t.0#heading=h.qtkpnhquytv6] lists tags that are searched via regex.
