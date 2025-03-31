5801b51414e99be42bf4fc7e7fe21fd88e7732ad
Owen Mathews <dembayago@allmail.net>
2025-03-31 10:43:37 -0700

    Script just for importing all phases of data with known filenames.

---
+++ Import all phases.R
@@ -0,0 +1,5 @@
+source("Import function.R")
+
+phase1 <- phase_import("../Cleaned datasets/ccc_compiled_20172020.tab")
+phase2 <- phase_import("../Cleaned datasets/ccc_compiled_20212024.tab")
+phase3 <- phase_import("../Cleaned datasets/ccc-phase3-public.tab")
